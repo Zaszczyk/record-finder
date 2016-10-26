1b6579083f874791bf0ab36388ad3c51582f58ab
marcin <marcin.szymanski@fitqbe.com>
2016-10-26 12:35:12 +0200

    small fixes

--- src/Finder/GeneralFinder.php
+++ src/Finder/GeneralFinder.php
@@ -7,12 +7,14 @@ class GeneralFinder extends AbstractFinder
     public function findRecordByDistance($distanceOfRecordInKm)
     {
         $finder = new DistanceFinder();
+        $finder->setData($this->data);
         return $finder->findRecordByDistance($distanceOfRecordInKm);
     }
 
     public function findRecordByTime($durationOfRecordInSeconds)
     {
         $finder = new DurationFinder();
+        $finder->setData($this->data);
         return $finder->findRecordByTime($durationOfRecordInSeconds);
     }
 }
